cf296937625f6e43a511ea4952a5634cdd53444f
zuo <2916902403@qq.com>
2021-05-24 23:50:14 +0800

    匿名结构体的初步了解

---
+++ exp5-24/exp5-24/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//匿名结构体
+//struct
+//{
+//	memberList;
+//}variableList;
+//struct
+//{
+//	int a;
+//	int b;
+//}s1 = { 10, 12 };
+//int main()
+//{
+//	printf("%d", s1.a);
+//	return 0;
+//}
+
+
