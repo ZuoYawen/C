686bcebe5085b24f8d03bc6bbc315d7d46927e57
ZuoYawen <81397279+ZuoYawen@users.noreply.github.com>
2021-05-28 00:08:22 +0800

    通讯录plus————全部独立完成，做了界面优化，提示信息汉化，排序方式由1种变成3种 ，断言处理，开辟内存由固定变为动态开辟

--- contact5-23/contact5-23/contact.h
+++ contact5-23/contact5-23/contact.h
@@ -1,12 +1,20 @@
+//较第一版的优化
+//1、界面优化 
+//2、通讯录存放的数据动态开辟，节省空间 
+//3、排序方式灵活
+//4、做了各种异常处理，增加健壮性
+
 #include <stdio.h>
 #include <string.h>
 #include <memory.h>
+#include <assert.h>
+#include <stdlib.h>
 
 #define NAME 8
 #define SEX 5
 #define PHONE 12
 #define ADDR 20
-#define NUM 100
+#define DEFAULT 5
 
 typedef struct people
 {
@@ -19,10 +27,14 @@ typedef struct people
 
 typedef struct contact
 {
-	people data[NUM];
-	int num;
+	people* data;
+	int num;//当前通讯录的有效人数（即目前实际放了多少人）
+	int mem;//当前通讯录的有效内存容量（即可以放多少人）
 }contact;
 
+//通讯录初始化
+void initContact(contact* c);
+//通讯录功能实现
 void add(contact* c);
 void show(contact* c);
 void del(contact* c);
@@ -30,3 +42,5 @@ void search(contact* c);
 void modify(contact* c);
 void clear(contact* c);
 void sort(contact* c);
+//通讯录销毁
+void destoryContacy(contact* c);
