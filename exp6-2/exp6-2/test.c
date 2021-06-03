9ca5634675f4e752a16b373fcb3c836b90eca4c3
ZuoYawen <81397279+ZuoYawen@users.noreply.github.com>
2021-06-03 23:34:39 +0800

    Update and rename tesy.c to test.c

--- exp6-2/exp6-2/test.c
+++ exp6-2/exp6-2/test.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//�����ո��ַ������������ַ�����+��-�ſ�ʼת��
-//���Ϸ������ַ�����,�����ַ�ASCIIΪ
+//跳过空格字符，遇上数字字符或者+、-才开始转换
+//遇上非数字字符结束,其他情况返回0
+//数字的ASCII码：
 int a_to_i(const char* string)
 {
 
