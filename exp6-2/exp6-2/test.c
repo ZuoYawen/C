96c5d66ddd9bfb8b6b82532baeedd65df8473c23
ZuoYawen <81397279+ZuoYawen@users.noreply.github.com>
2021-06-06 23:35:25 +0800

    Update test.c

--- exp6-2/exp6-2/test.c
+++ exp6-2/exp6-2/test.c
@@ -1,13 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 //跳过空格字符，遇上数字字符或者+、-才开始转换
 //遇上非数字字符结束,其他情况返回0
-//数字的ASCII码：48-57
+//数字的ASCII码：48-57对应0-9
 int a_to_i(const char* string)
 {
-
+	assert(string);
+	if (*string > 57 || *string < 48)
+	{
+		
+	}
 }
 int main()
 {
